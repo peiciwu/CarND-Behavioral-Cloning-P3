e78d804c8e4816bdc339bc1df5e6333dc85178c6
Pei-Ci Wu <peiciwu@gmail.com>
2017-06-14 22:35:54 +0000

    Refactoring - use seperate functions.
    	modified:   model.py

--- model.py
+++ model.py
@@ -3,8 +3,13 @@ import cv2
 import numpy as np
 import sklearn
 from random import shuffle
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.convolutional import Convolution2D
+#from keras.layers.advanced_activations import ELU
+from keras.layers import Cropping2D
 
-def readDrivingLog(dir_name, samples):
+def readDrivingLog(samples, dir_name):
     with open(dir_name+'/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
         next(reader, None) # skip the header
@@ -30,6 +35,55 @@ def generator(samples, batch_size=32):
             X_train = np.array(images)
             y_train = np.array(steerings)
             yield sklearn.utils.shuffle(X_train, y_train)
+
+def fit_model(train_generator, validation_generator, num_train_samples, num_validation_samples, num_epoch):
+    """
+    Use Nvidia model
+    """
+    model = Sequential()
+    ### Normalization
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+    ### Image cropping layer. Crop 70 pixels from the top and 25 pixels from the bottom.
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+
+    ### Add three convolutional layers with a 5x5 kerenl and a 2x2 stride. Output depth is 24, 36, and 48, respectively.
+    #model.add(Convolutional2D(24,5,5, subsample=(2,2), border_mode='valid', W_regularizer=l2(0.001)))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
+    #model.add(ELU())
+    model.add(Convolution2D(36,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
+    #model.add(ELU())
+    model.add(Convolution2D(48,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
+    #model.add(ELU())
+
+    ### Add two convolutional layers with a 3x3 kerenl. Output depth are both 64.
+    model.add(Convolution2D(64,3,3, border_mode='valid', activation='relu'))
+    #model.add(ELU())
+    model.add(Convolution2D(64,3,3, border_mode='valid', activation='relu'))
+    #model.add(ELU())
+
+    model.add(Flatten())
+
+    ### Fully connected layer. Output = 100
+    model.add(Dense(100))
+    #model.add(Activation('relu'))
+    ### Fully connected layer. Output = 50
+    model.add(Dense(50))
+    #model.add(Activation('relu'))
+    ### Fully connected layer. Output = 10
+    model.add(Dense(10))
+    #model.add(Activation('relu'))
+
+    ### Final output layer
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam') # Mean sqaure error and adam optimizer.
+    history_object = model.fit_generator(train_generator,
+                                         samples_per_epoch=num_train_samples,
+                                         validation_data=validation_generator,
+                                         nb_val_samples=num_validation_samples, nb_epoch=5, verbose=1)
+    model.save('model.h5')
+
+    return history_object
 				
     
 ###########################
@@ -39,68 +93,24 @@ def generator(samples, batch_size=32):
 # - Collect my own data.
 ###########################
 
+#################################################################
+# Start Main function
+#################################################################
+
 samples = []
-readDrivingLog('data', samples) # Read from samples data provided by the class
+readDrivingLog(samples, dir_name='data') # Read from samples data provided by the class
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 ### Use generator
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+BATCH_SIZE = 32
+train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
 
 ### Model building
-from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Activation
-from keras.layers.convolutional import Convolution2D
-#from keras.layers.advanced_activations import ELU
-from keras.layers import Cropping2D
-
-"""
-Nvidia model
-"""
-model = Sequential()
-### Normalization
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-### Image cropping layer. Crop 70 pixels from the top and 25 pixels from the bottom.
-model.add(Cropping2D(cropping=((70,25), (0,0))))
-
-### Add three convolutional layers with a 5x5 kerenl and a 2x2 stride. Output depth is 24, 36, and 48, respectively.
-#model.add(Convolutional2D(24,5,5, subsample=(2,2), border_mode='valid', W_regularizer=l2(0.001)))
-model.add(Convolution2D(24,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
-#model.add(ELU())
-model.add(Convolution2D(36,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
-#model.add(ELU())
-model.add(Convolution2D(48,5,5, subsample=(2,2), border_mode='valid', activation='relu'))
-#model.add(ELU())
-
-### Add two convolutional layers with a 3x3 kerenl. Output depth are both 64.
-model.add(Convolution2D(64,3,3, border_mode='valid', activation='relu'))
-#model.add(ELU())
-model.add(Convolution2D(64,3,3, border_mode='valid', activation='relu'))
-#model.add(ELU())
-
-model.add(Flatten())
-
-### Fully connected layer. Output = 100
-model.add(Dense(100))
-#model.add(Activation('relu'))
-### Fully connected layer. Output = 50
-model.add(Dense(50))
-#model.add(Activation('relu'))
-### Fully connected layer. Output = 10
-model.add(Dense(10))
-#model.add(Activation('relu'))
-
-### Final output layer
-model.add(Dense(1))
-
-
-model.compile(loss='mse', optimizer='adam') # Mean sqaure error and adam optimizer.
-history_object = model.fit_generator(train_generator,
-        samples_per_epoch=len(train_samples),
-        validation_data=validation_generator,
-        nb_val_samples=len(validation_samples), nb_epoch=5, verbose=1)
+EPOCH = 5
+history_object = fit_model(train_generator, validation_generator, len(train_samples), len(validation_samples), EPOCH)
 
 ### Plot the training and validation loss for each epoch
 import matplotlib
@@ -114,5 +124,4 @@ plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.savefig('history.png')
 
-model.save('model.h5')
 
